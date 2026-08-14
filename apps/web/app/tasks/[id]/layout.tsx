ae0354149889fa450c7d148d59806ad3ed8a607a
rewochdd <jaggercao0@gmail.com>
2026-08-15 01:22:01 +0930

    Load contract worker identity styles

--- apps/web/app/tasks/[id]/layout.tsx
+++ apps/web/app/tasks/[id]/layout.tsx
@@ -1,6 +1,7 @@
 import "./contract-exchange.css";
 import "./contract-command.css";
 import "./contract-atmosphere.css";
+import "./contract-identity.css";
 
 export default function ContractLayout({
   children,
